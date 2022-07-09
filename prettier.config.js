ac00317a4233bfe47b8e52716e6e1af786a52c2a
Dela Anthonio <dela.anthonio@invitae.com>
2022-07-09 11:46:38 -0400

    chore: add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'off',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  printWidth: 90,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
